37b8554b5a34e2161dda6e9ad52a439ec7412dc2
VuNgNgocBao04 <jackoldbao@gmail.com>
2024-12-02 10:30:44 +0700

    addTb

--- FCFS.cpp
+++ FCFS.cpp
@@ -35,6 +35,14 @@ int main(){
 	for(int i=0;i<n;i++) {
 		cout<<"Process "<<i+1<<" wait time: "<<wait[i]<<" save time: "<<save[i]<<endl;
 	}
+	float Wtb=0;
+	float Stb=0;
+	for(int i=0;i<n;i++) {
+		Wtb+=wait[i];
+		Stb+=save[i];
+	}
+	cout<<" average wait time: "<<fixed<<setprecision(2)<<Wtb/n*1.0
+		<<" average save time: "<<fixed<<setprecision(2)<<Stb/n*1.0;
 	delete[] p;
 	delete[] end;
 	delete[] wait;
